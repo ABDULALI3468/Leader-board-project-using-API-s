e71cc49a6e11bd9fd721097d887f6a36a7e009ba
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-11-21 22:41:48 +0500

    Added slider and enhanced responsive styles.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,76 @@
-import './styles/styles.css';
-import party from 'party-js';
-import getData from './modules/getData';
-import test from './modules/test';
-import Score from './modules/score.js';
+import "./styles/styles.css";
+import party from "party-js";
+import getData from "./modules/getData";
+import test from "./modules/test";
+import Score from "./modules/score.js";
+import { inValidInput } from "./modules/validation.js";
 
-const scoreList = document.querySelector('.scoreList');
-const submit = document.querySelector('.submit');
-const refreshBtn = document.querySelector('.scoreRefresh');
-const nameInput = document.querySelector('.nameInput');
-const scoreInput = document.querySelector('.scoreInput');
+const scoreList = document.querySelector(".scoreList");
+const submit = document.querySelector(".submit");
+const refreshBtn = document.querySelector(".scoreRefresh");
+const nameInput = document.querySelector(".nameInput");
+const scoreInput = document.querySelector(".scoreInput");
+const hamburger = document.querySelector(".ham-container");
+const crossicon = document.querySelector(".cross-icon");
+const inputScoreContainer = document.querySelector(".addScores");
 
-refreshBtn.addEventListener('click', async (e) => {
+refreshBtn.addEventListener("click", async (e) => {
   party.confetti(document.body, {
     count: party.variation.range(100, 200),
   });
   e.preventDefault();
-  scoreList.innerHTML = '';
+  scoreList.innerHTML = "";
   getData();
 });
 
-submit.addEventListener('click', (e) => {
-  party.confetti(document.body, {
-    count: party.variation.range(100, 200),
-  });
-  const list = new Score(nameInput.value, scoreInput.value);
-  e.preventDefault();
-  test(list);
-  scoreList.innerHTML = '';
-  document.querySelector('.nameInput').value = '';
-  document.querySelector('.scoreInput').value = '';
-  setTimeout(getData, 500);
+submit.addEventListener("click", (e) => {
+  if (nameInput.value && scoreInput.value) {
+    party.confetti(document.body, {
+      count: party.variation.range(100, 200),
+    });
+    const list = new Score(nameInput.value, scoreInput.value);
+    e.preventDefault();
+    test(list);
+    scoreList.innerHTML = "";
+    nameInput.value = "";
+    scoreInput.value = "";
+
+    nameInput.style.border = "none";
+    nameInput.style.placeholder = "Your name";
+    nameInput.style.removeProperty("--color");
+
+    scoreInput.style.border = "none";
+    scoreInput.style.placeholder = "Your score";
+    scoreInput.style.removeProperty("--color");
+    inputScoreContainer.setAttribute("data-visible", "false");
+    document.body.style.overflow = "auto";
+    setTimeout(getData, 500);
+  } else {
+    inValidInput(nameInput, scoreInput);
+
+    nameInput.placeholder = "Please type something!";
+    nameInput.style.border = "2px solid red";
+    nameInput.style.setProperty("--color", "red");
+
+    scoreInput.placeholder = "Please type something!";
+    scoreInput.style.border = "2px solid red";
+    scoreInput.style.setProperty("--color", "red");
+  }
 });
 
-window.addEventListener('DOMContentLoaded', getData);
+hamburger.addEventListener("click", () => {
+  document.body.style.overflow = "hidden";
+  const visibility = inputScoreContainer.getAttribute("data-visible");
+  if (visibility === "false") {
+    inputScoreContainer.setAttribute("data-visible", "true");
+  } else if (visibility === "true") {
+    inputScoreContainer.setAttribute("data-visible", "false");
+  }
+});
+
+crossicon.addEventListener("click", () => {
+  inputScoreContainer.setAttribute("data-visible", "false");
+  document.body.style.overflow = "auto";
+});
+
+window.addEventListener("DOMContentLoaded", getData);
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,50 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const svgToMiniDataURI = require("mini-svg-data-uri");
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
+  mode: "development",
+  entry: "./src/index.js",
   devServer: {
-    static: './docs',
+    static: "./docs",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
   ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'docs'),
+    filename: "app.[hash].js",
+    path: path.resolve(__dirname, "docs"),
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.png$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 20 * 1024, // 4kb
+          },
+        },
+      },
+      // {
+      //   test: /\.html$/,
+      //   use: ["html-loader"],
+      // },
     ],
   },
 };
