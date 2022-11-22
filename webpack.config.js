9cb8ed9c5e9819afb3a6e0659d04d23a096646b3
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-11-22 11:12:14 +0500

    Corrected Linters!

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,39 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const svgToMiniDataURI = require("mini-svg-data-uri");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   devServer: {
-    static: "./docs",
+    static: './docs',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: "app.[hash].js",
-    path: path.resolve(__dirname, "docs"),
+    filename: 'app.[hash].js',
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.png$/,
-        type: "asset",
+        type: 'asset',
         parser: {
           dataUrlCondition: {
             maxSize: 20 * 1024, // 4kb
